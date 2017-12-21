d0ec3703e82cc88b50ef0638c0658a3868c43048
DangHa <dangha997@gmail.com>
2017-12-21 15:58:08 +0700

    Tao react app

---
+++ static/app/src/DangHa.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DangHa extends Component {
+    tinhtong(){
+
+    }
+      render() {
+        return (
+          <h1>Dang Ha</h1>
+        );
+      }
+}
+
+export default DangHa;
